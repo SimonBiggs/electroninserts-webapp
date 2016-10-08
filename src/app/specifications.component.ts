bbd1dc8f3d870b1b8f6b2486ee05e5a7f5444167
SimonBiggs <mail@simonbiggs.net>
2016-10-08 23:56:25 +1100

    begin building specifications component

--- src/app/specifications.component.ts
+++ src/app/specifications.component.ts
@@ -7,13 +7,57 @@ import { TitleService } from './title.service'
   templateUrl: './specifications.component.html'
 })
 export class SpecificationsComponent implements OnInit {
+  machines: string[];
+  specifications = {};
+  currentMachine: string;
 
   constructor(
     private myTitleService: TitleService
   ) {}
+
+  changeCurrentMachine(machine: string) {
+    this.currentMachine = machine;
+  }
   
   ngOnInit() {
     this.myTitleService.setTitle('Specifications');
+    this.machines = ["2619", "2694", "Another"];
+
+    this.specifications = {
+      "2619": {
+        "model": "Elekta Synergy",
+        "energy": [6, 9, 12],
+        "R50": {
+          6: 0,
+          9: 0,
+          12: 0
+        },
+        "applicator": ["10X10", "6X6"],
+        "ssd": [100]
+      },
+      "2694": {
+        "model": "Elekta Synergy",
+        "energy": [6, 9, 12],
+        "R50": {
+          6: 0,
+          9: 0,
+          12: 0
+        },
+        "applicator": ["14X14", "6X6"],
+        "ssd": [100]
+      },
+      "Another": {
+        "model": "Varian True Beam",
+        "energy": [6, 8, 10],
+        "R50": {
+          6: 0,
+          8: 0,
+          10: 0
+        },
+        "applicator": ["10X10", "6X6"],
+        "ssd": [100]
+      }
+    }
   }
 
 }
